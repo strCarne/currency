884e2f0b77da228caab5b5b404fbcc0d51e492b2
strCarne <semionvoevoda@gmail.com>
2024-10-01 18:00:33 +0300

    feat: define client interface for NBRB

---
+++ internal/clients/nbrb/client.go
@@ -0,0 +1,41 @@
+package nbrb
+
+import (
+	"context"
+
+	"github.com/strCarne/currency/internal/models/schema"
+	"github.com/strCarne/currency/pkg/models"
+)
+
+const nbrbRatesRawURL = "https://api.nbrb.by/exrates/rates"
+
+type Periodicity int
+
+const (
+	Dayly Periodicity = iota
+	Monthly
+)
+
+type ParamMode int
+
+const (
+	Internal ParamMode = iota
+	DigitISO4217
+	AlphaISO4217
+)
+
+type Client interface {
+	GetRates(
+		ctx context.Context,
+		onDate *models.Date,
+		periodicity Periodicity,
+		paramMode *ParamMode,
+	) ([]schema.Rate, error)
+
+	GetRate(
+		ctx context.Context,
+		curID int,
+		onDate *models.Date,
+		paramMode *ParamMode,
+	) (*schema.Rate, error)
+}
