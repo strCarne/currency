aced5e6ac5cf729f549000a7e50543a8b0113206
strCarne <semionvoevoda@gmail.com>
2024-10-01 16:18:26 +0300

    feat: set up orm

---
+++ pkg/db/gorm.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// SAFETY:
+// gorm.DB is thread-safe, so it is ok to use it in multiple goroutines
+// and be a global variable.
+//
+//nolint:gochecknoglobals
+var connection *gorm.DB
+
+func InitializeGORM() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		panic("DATABASE_URL is not set")
+	}
+
+	//nolint:exhaustruct
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	connection = conn
+}
+
+func Connection() *gorm.DB {
+	if connection == nil {
+		panic("connection is not initialized (use InitializeGORM in main func)")
+	}
+
+	return connection
+}
