3759d93e7fdb3676d1b3043ead55a9cfcee230a3
strCarne <semionvoevoda@gmail.com>
2024-10-01 23:37:55 +0300

    fix: fix struct tags for Rate

--- internal/schema/rate.go
+++ internal/schema/rate.go
@@ -7,10 +7,10 @@ import (
 //nolint:tagliatelle
 type Rate struct {
 	gorm.Model   `json:"-"`
-	CurID        int     `gorm:"not null;uniqueIndex:idx_unique_curid_date"             json:"Cur_ID"`
-	Date         string  `gorm:"varchar(20);not null;uniqueIndex:idx_unique_curid_date" json:"Date"`
-	Abbreviation string  `gorm:"varchar(5);not null"                                    json:"Cur_Abbreviation"`
-	Scale        int     `gorm:"not null"                                               json:"Cur_Scale"`
-	Name         string  `gorm:"varchar(64);not null"                                   json:"Cur_Name"`
-	OfficialRate float64 `gorm:"not null"                                               json:"Cur_OfficialRate"`
+	CurID        int     `gorm:"not null;uniqueIndex:idx_unique_curid_date"                  json:"Cur_ID"`
+	Date         string  `gorm:"type:varchar(20);not null;uniqueIndex:idx_unique_curid_date" json:"Date"`
+	Abbreviation string  `gorm:"type:varchar(5);not null"                                    json:"Cur_Abbreviation"`
+	Scale        int     `gorm:"not null"                                                    json:"Cur_Scale"`
+	Name         string  `gorm:"type:varchar(64);not null"                                   json:"Cur_Name"`
+	OfficialRate float64 `gorm:"not null"                                                    json:"Cur_OfficialRate"`
 }
