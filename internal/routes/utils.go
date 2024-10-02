18fb4dae6ea702af2336f0de8014d35bed94c031
strCarne <semionvoevoda@gmail.com>
2024-10-02 20:58:33 +0300

    feat: make custom Handler context and wrapper for echo

--- internal/routes/utils.go
+++ internal/routes/utils.go
@@ -2,8 +2,28 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/strCarne/currency/internal/controllers"
 	"github.com/strCarne/currency/pkg/shared"
+	"gorm.io/gorm"
 )
+
+type Context struct {
+	EchoCtx  echo.Context
+	ConnPool *gorm.DB
+	Poller   *controllers.NBRBPoller
+}
+
+type HandlerFunc func(Context) error
+
+func Wrap(handler HandlerFunc, connPool *gorm.DB, poller *controllers.NBRBPoller) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		return handler(Context{
+			EchoCtx:  ctx,
+			ConnPool: connPool,
+			Poller:   poller,
+		})
+	}
+}
 
 func jsonResponse[T any](ctx echo.Context, code int, message string, payload T) error {
 	response := shared.Response[T]{
