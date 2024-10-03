dee1501c2bea108449b1bc942ec83178d3bedd88
strCarne <semionvoevoda@gmail.com>
2024-10-03 10:36:52 +0300

    fix: fix MustAddress

--- internal/setup/address.go
+++ internal/setup/address.go
@@ -8,27 +8,27 @@ import (
 )
 
 func MustAddress() string {
-	ipAddr := os.Getenv("ADDRESS")
+	ipAddr := os.Getenv("ECHO_ADDRESS")
 	if ipAddr == "" {
-		panic("ADDRESS must be set")
+		panic("ECHO_ADDRESS must be set")
 	}
 
 	if net.ParseIP(ipAddr) == nil {
-		panic("ADDRESS must be valid ip-address")
+		panic("ECHO_ADDRESS must be valid ip-address")
 	}
 
-	portStr := os.Getenv("PORT")
+	portStr := os.Getenv("ECHO_PORT")
 	if portStr == "" {
-		panic("PORT must be set")
+		panic("ECHO_PORT must be set")
 	}
 
-	intPort, err := strconv.Atoi(os.Getenv("PORT"))
+	intPort, err := strconv.Atoi(portStr)
 	if err != nil {
-		panic("PORT must be integer")
+		panic("ECHO_PORT must be integer")
 	}
 
 	if intPort < 0 || intPort > math.MaxUint16 {
-		panic("PORT must be in range [0, 65535]")
+		panic("ECHO_PORT must be in range [0, 65535]")
 	}
 
 	return ipAddr + ":" + portStr
